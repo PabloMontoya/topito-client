7d1cb1fb0e5df0fedac3c76163ac909ac639a9c1
PabloMontoya <pablomontoya392@gmail.com>
2023-11-30 02:03:04 -0700

    Add test for the logout functionality

--- src/components/Navbar/Navbar.test.js
+++ src/components/Navbar/Navbar.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './Navbar';
 
@@ -20,5 +20,26 @@ describe('Navbar', () => {
       </Router>
     );
     expect(screen.queryByRole('button')).not.toBeInTheDocument();
+  });
+
+  it('should sucessfully unauthenticate and clear session data when user logs out', () => {
+    // Mock sessionStorage.clear()
+    Storage.prototype.clear = jest.fn();
+
+    render(
+      <Router>
+        <Navbar isAuthenticated={true} setIsAuthenticated={jest.fn()} />
+      </Router>
+    );
+
+    // Simulate user clicking on the profile icon button
+    const profileIconButton = screen.getByRole('button');
+    fireEvent.click(profileIconButton);
+
+    // Simulate user clicking on the logout button
+    const logoutButton = screen.getByText(/logout/i);
+    fireEvent.click(logoutButton);
+
+    expect(sessionStorage.clear).toHaveBeenCalled();
   });
 });
