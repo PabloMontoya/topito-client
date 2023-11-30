d636ed2b11828dc15372e9f884111b45bd236e0a
PabloMontoya <pablomontoya392@gmail.com>
2023-11-30 01:51:37 -0700

    Migrate Singup, Login and Home pages to MUI

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,18 +1,37 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link as RouterLink } from 'react-router-dom';
+import { Typography, Container, Box, Link } from '@mui/material';
 import LoginForm from '../../components/LoginForm/LoginForm';
-import './LoginPage.css';
 
 const LoginPage = ({ setIsAuthenticated }) => {
   return (
-    <div>
-      <h1>Login to Your Account</h1>
-      <img src="/topito.png" alt="Topito Logo" className="login-logo" />
+    <Container
+      component="main"
+      maxWidth="sm"
+      sx={{ textAlign: 'center', padding: '20px' }}
+    >
+      <Typography
+        variant="h4"
+        component="h1"
+        sx={{ color: 'white', marginBottom: '20px' }}
+      >
+        Login to Your Account
+      </Typography>
+      <Box sx={{ maxWidth: 170, margin: '0 auto', padding: '20px' }}>
+        <img
+          src="/topito.png"
+          alt="Topito Logo"
+          style={{ width: '100%', height: 'auto' }}
+        />
+      </Box>
       <LoginForm setIsAuthenticated={setIsAuthenticated} />
-      <p>
-        Don't have an account? <Link to="/">Sign up</Link>
-      </p>
-    </div>
+      <Typography variant="body1" sx={{ color: 'white', marginTop: '20px' }}>
+        Don't have an account?{' '}
+        <Link component={RouterLink} to="/" sx={{ color: 'inherit' }}>
+          Sign up
+        </Link>
+      </Typography>
+    </Container>
   );
 };
 
